ca4cfb1f2dd8f18e6258374cd7d35dfaf96fa2d3
appu-46 <ap958461@gmail.com>
2025-12-07 09:44:04 +0530

    Type safe OAuth params

--- src/services/OAuth.tsx
+++ src/services/OAuth.tsx
@@ -2,19 +2,17 @@ export function oauthSignIn() {
   const OAUTH_CLIENT_ID = import.meta.env.VITE_OAUTH_CLIENT_ID
   const REDIRECTION_URI = 'http://localhost:3000/'
 
-  console.log(OAUTH_CLIENT_ID)
-
   // Google's OAuth 2.0 endpoint for requesting an access token
-  let oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
+  const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
 
   // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-  let form = document.createElement('form')
+  const form = document.createElement('form')
   form.setAttribute('method', 'GET') // Send as a GET request.
   form.setAttribute('action', oauth2Endpoint)
   form.setAttribute('target', '_blank')
 
   // Parameters to pass to OAuth 2.0 endpoint.
-  let params = {
+  const params = {
     client_id: OAUTH_CLIENT_ID,
     redirect_uri: REDIRECTION_URI,
     response_type: 'token',
@@ -24,11 +22,12 @@ export function oauthSignIn() {
   }
 
   // Add form parameters as hidden input values.
-  for (let p in params) {
-    let input = document.createElement('input')
+  for (const p in params) {
+    const key = p as keyof typeof params
+    const input = document.createElement('input')
     input.setAttribute('type', 'hidden')
     input.setAttribute('name', p)
-    input.setAttribute('value', params[p])
+    input.setAttribute('value', params[key])
     form.appendChild(input)
   }
 
